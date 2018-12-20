f18755fe3864b8f49efdd201b52e95aa24fdf3ba
MichaelLafosse99 <michaellafosse99@gmail.com>
2018-12-20 17:36:17 -0600

    commented stuff

--- Dependencies/MathLib/Include/Vector2.h
+++ Dependencies/MathLib/Include/Vector2.h
@@ -4,19 +4,31 @@
 class Vector2
 {
 private:
+	//X and Y positions
 	float xPos;
 	float yPos;
 
 public:
+	//Constructor to initialize X and Y
 	Vector2(float x, float y);
 	Vector2();
 
+	//Getx() and Gety() returns their positions
 	float Gety();
 	float Getx();
+
+	//Returns the length of a vector
 	float Magnitude();
+
+	//Finds the distance between two vectors
 	float Distance(Vector2 other);
+
+	float DotProduct(Vector2 other);
+
+	//Modifies the vector's magnitude
 	Vector2 Normalize();
 
+	//Operators to add, subtract, multiply, and equality.
 	Vector2 operator + (Vector2& rhs);
 	Vector2 operator - (Vector2& rhs);
 	Vector2 operator * (float& rhs);
--- Dependencies/MathLib/Include/Vector3.h
+++ Dependencies/MathLib/Include/Vector3.h
@@ -4,21 +4,38 @@
 class Vector3
 {
 private:
+	//X, Y, and Z positions
 	float xPos;
 	float yPos;
 	float zPos;
 
 public:
+	//Constructor to initialize X, Y, and Z
 	Vector3(float x, float y, float z);
 	Vector3();
 
+	//Getx(), Gety(), Getz() returns their positions
 	float Gety();
 	float Getx();
 	float Getz();
+
+	//Returns the length of a vector
 	float Magnitude();
+
+	//Finds the distance between two vectors
 	float Distance(Vector3 other);
+
+
+	float DotProduct(Vector3 other);
+	
+	//Modifies the vector's magnitude
 	Vector3 Normalize();
 
+	//Returns a third vector that is perpendicular to both of
+	//the original vectors
+	Vector3 CrossProduct(Vector3 other);
+
+	//Operators to add, subtract, multiply, and equality.
 	Vector3 operator + (Vector3& rhs);
 	Vector3 operator - (Vector3& rhs);
 	Vector3 operator * (float& rhs);
